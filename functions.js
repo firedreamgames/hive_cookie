cdb50c8ceab85ed0c2334cfa395bb533f50d9f4d
firedreamgames <34807351+firedreamgames@users.noreply.github.com>
2021-11-29 17:06:47 +0300

    Add files via upload
    .message function added

--- functions.js
+++ functions.js
@@ -1,14 +1,12 @@
 var hive = require('@hiveio/hive-js');
-const fs = require("fs")
+const fs = require("fs");
+const { builtinModules } = require('module');
+
+
+
+
 module.exports={
-    
-    getAccounts:function(user,cb){
-        hive.api.getAccounts(user, function(err, response){
-         cb(response) 
-            
-        })
-    },
-    getRandomLine:function (filename, callback){
+    getRandomLine:function(filename, callback){
         fs.readFile(filename, "utf-8", function(err, data){
           if(err) {
               throw err;
@@ -24,8 +22,45 @@ module.exports={
           // invoke the callback with our line
           callback(line);
        })
-      }
+      },
+    
+    getAccounts:function(user,cb){
+        hive.api.getAccounts(user, function(err, response){
+         cb(response) 
+            
+        })
+    },
+    
+    messageSend:function(voter,percent,line){
+        
+       
+        if(percent>=0){
+           
+            return (
+                "Dear @"+voter+"\n"+
+                "Thank you for your %"+(percent/100)+" upvote"+"\n"+
+                "Here is your fortune cookie below"+"\n"+
+                "****************************************************"+"\n"+                           
+                line+"\n"+
+                "****************************************************"+"\n"
+                )
+                
+        }else{
+            return(
+                "Dear @"+voter+"\n"+
+                "Sorry for your %"+(percent/100)+" downvote"+"\n"+
+                "It seems you don't like fortune cookies"+"\n"+
+                "Anyway I send you a fortune cookie below"+"\n"+
+                "****************************************************"+"\n"+                           
+                line+"\n"+
+                "****************************************************"+"\n"
+            )
+
+            }
+        
+    }
    
 
 }
+
 
